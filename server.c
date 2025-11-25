9ac0952780f0efec62ab9641d2b171ba3df4d8aa
Rixda9 <78431473+Rixda9@users.noreply.github.com>
2025-11-25 15:48:39 +0100

    Add basic struct setup and bind function

---
+++ server.c
@@ -0,0 +1,25 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+int main() {
+
+  struct sockaddr_in address;
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(8080);
+
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+    perror("Socket failed\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("Binding failed\n");
+    exit(EXIT_FAILURE);
+  }
+  return 0;
+}
